be30d9621da14d49a02b745c623f6b59eedb07bd
Yonk Shi <yonkshi@air-kamila.lan>
2019-07-16 22:09:16 +0200

    Added self attention for backbone

--- Makefile
+++ Makefile
@@ -3,14 +3,11 @@ up:
 	git commit -m "AUTO: Small Fix"
 	git push
 
-#run:
-#	python3 train.py
+run:
+	python3 train.py
 
 run_gpu:
 	python3 train.py --gpu
-
-hp_search:
-	python3 hp_search.py --gpu --conv_neighbourhood 2 --use_uber_trick --use_conv_z_attr --hp_search_coarse --z_pres no_prior
 
 sync:
 	rsync -arvu --exclude=logs_v2/ --exclude=logs/ --exclude=spair/data/* --exclude=data/* -e ssh . naturalreaders:spair_pytorch
@@ -18,8 +15,6 @@ sync:
 tb:
 	tensorboard --logdir logs/ --host 0.0.0.0 --port 8081
 
-tb_hp_search:
-	tensorboard --logdir logs/hp_search/ --host 0.0.0.0 --port 8081
 
 overnight:
 	python3 train.py --gpu || true
@@ -34,19 +29,3 @@ test_new_features:
 	python3 train.py --gpu --backbone_self_attention || true
 	python3 train.py --gpu --use_uber_trick --use_conv_z_attr --z_pres no_prior --conv_neighbourhood 2 --backbone_self_attention || true
 
-format: ## Run pre-commit hooks to format code
-	 pre-commit run --all-files
-
-args ?= -vvv --cov ssd
-
-build: ## Build docker image
-	docker build -f Dockerfile -t spair:latest .
-
-docker_args ?= --gpus all  --volume $(shell pwd):/app --volume $(shell pwd)/data:/app/data
-
-shell: ## Run poetry shell
-	docker run -it --rm $(docker_args) --entrypoint /bin/bash spair:latest
-
-spair_args ?=
-run: ## Run model
-	docker run --rm $(docker_args) spair:latest $(spair_args)
--- train.py
+++ train.py
@@ -1,26 +1,34 @@
-import argparse
-import time
 from datetime import datetime
+import logging
+import sys
+import argparse
+import os
+import time
+import requests
+import gc
 
 import numpy as np
+import cv2
 import torch
-from coolname import generate_slug
+from torch import nn, optim
+from torch.utils import data as torch_data
 from tensorboardX import SummaryWriter
+from coolname import generate_slug
 from torch import optim
 from tqdm.auto import tqdm
 
+from spair.models import Spair, ConvSpair
 from spair import config as cfg
+from spair.dataloader import SimpleScatteredMNISTDataset
+from spair.manager import RunManager
 from spair import debug_tools
 from spair import metric
-from spair.dataloader import SimpleScatteredMNISTDataset
 from spair.logging import *
-from spair.manager import RunManager
-from spair.models import ConvSpair, Spair
 
 
 def main():
-    run_name = datetime.today().strftime("%b-%d") + "-" + generate_slug(2)
-    log_path = "logs/%s" % run_name
+    run_name = datetime.today().strftime('%b-%d') + '-' + generate_slug(2)
+    log_path = 'logs/%s' % run_name
 
     args = parse_args(log_path)
     # Setup TensorboardX writer
@@ -29,29 +37,23 @@ def main():
     init_logger(log_path)
 
     device = torch.device("cuda" if (torch.cuda.is_available() and args.gpu) else "cpu")
-    dataset_path = "data/" + args.dataset_filename
+    dataset_path = 'data/' + args.dataset_filename
     dataset_subset_name = args.dataset_subset
     data = SimpleScatteredMNISTDataset(dataset_path, dataset_subset_name)
 
-    run_manager = RunManager(
-        run_name=run_name, dataset=data, device=device, writer=writer, run_args=args
-    )
+    run_manager = RunManager(run_name=run_name, dataset=data, device=device, writer=writer, run_args=args)
 
     if not cfg.IS_LOCAL:
         try:
             train(run_manager)
         except Exception as e:
-            telegram_yonk(
-                "An error had occured:{}, step:{}".format(
-                    run_name, RunManager.global_step
-                )
-            )
+            telegram_yonk('An error had occured:{}, step:{}'.format(run_name, RunManager.global_step) )
             raise e
     else:
         train(run_manager)
 
-
 def train(run_manager):
+
     image_shape = cfg.INPUT_IMAGE_SHAPE
     device = run_manager.device
     writer = run_manager.writer
@@ -66,7 +68,8 @@ def train(run_manager):
         spair_net = Spair(image_shape)
     else:
         spair_net = ConvSpair(image_shape)
-        print("Running with CONV spair")
+        print('Running with CONV spair')
+
 
     if continue_training(run_manager.run_args) is not None:
         model_dict, global_step = continue_training(run_manager.run_args)
@@ -81,7 +84,7 @@ def train(run_manager):
     # Main training loop
 
     for global_step, batch in tqdm(
-        run_manager.iterate_data(), total=run_manager.run_args.max_iter
+            run_manager.iterate_data(), total=run_manager.run_args.max_iter
     ):
 
         x_image, y_bbox, y_digit_count = batch
@@ -91,48 +94,61 @@ def train(run_manager):
 
         # log("Iteration", global_step)
         debug_tools.benchmark_init()
-        spair_optim.zero_grad()
+
         loss, out_img, z_where, z_pres = spair_net(x_image)
         # log('===> loss:', '{:.4f}'.format(loss.item()))
 
-        loss.backward(retain_graph=True)
+        loss.backward(retain_graph = True)
         spair_optim.step()
 
         # Log average precision metric every 5 step after 1000 iterations (when trainig_wheel is off)
-        if global_step > 1000 and global_step % 100 == 0:  # global_step > 1000 and
-            meanAP = metric.mAP_igiveup(z_where, z_pres, y_bbox, y_digit_count)
-            log("Bbox Average Precision:", meanAP.item())
-            writer.add_scalar("accuracy/bbox_average_precision", meanAP, global_step)
+        if global_step > 1000 and global_step % 5 == 0: # iteration > 1000 and
+            meanAP = metric.mAP(z_where, z_pres, y_bbox, y_digit_count)
+            log('Bbox Average Precision:', meanAP.item())
+            writer.add_scalar('accuracy/bbox_average_precision', meanAP, global_step)
 
             count_accuracy = metric.object_count_accuracy(z_pres, y_digit_count)
-            writer.add_scalar(
-                "accuracy/object_count_accuracy", count_accuracy, global_step
-            )
+            writer.add_scalar('accuracy/object_count_accuracy', count_accuracy, global_step)
 
         # Save model
-        if global_step % 100 == 0 and global_step > 0:
+        if global_step % 50 == 0 and global_step > 0:
             # logging stuff
             image_out = out_img[0].detach()
             image_in = x_image[0].detach()
             combined_image = torch.cat([image_in, image_out], dim=2)
-            writer.add_image("SPAIR input_output", combined_image, global_step)
+            writer.add_image('SPAIR input_output', combined_image, global_step)
 
             duration = time.time() - benchmark_time
-            writer.add_scalar("misc/time_taken_per_100_batches", duration, global_step)
-            check_point_name = "step_%d.pkl" % global_step
-            cp_dir = os.path.join(run_log_path, "checkpoints")
+            writer.add_scalar('misc/time_taken_per_100_batches', duration, global_step)
+            check_point_name = 'step_%d.pkl' % global_step
+            cp_dir = os.path.join(run_log_path, 'checkpoints')
             os.makedirs(cp_dir, exist_ok=True)
-            save_path = os.path.join(run_log_path, "checkpoints", check_point_name)
+            save_path = os.path.join(run_log_path, 'checkpoints', check_point_name)
             torch.save(spair_net.state_dict(), save_path)
             benchmark_time = time.time()
 
+        if global_step % 1 == 0:
+            should_break = False
+            count = 0
+            for obj in gc.get_objects():
+                try:
+                    if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
+                        # print(type(obj), obj.size(), obj.name)
+                        should_break = True
+                        count += 1
+                except:
+                    pass
+
+            print('total tensors alive', count)
+            # if should_break: import ipdb; ipdb.set_trace()
+
         # torch.cuda.empty_cache()
 
-    telegram_yonk("Run completed! name:{}".format(run_manager.run_name))
+
+    telegram_yonk('Run completed! name:{}'.format(run_manager.run_name))
 
     for name, param in spair_net.named_children():
         print(name)
-
 
 def continue_training(args):
     if not args.continue_from:
@@ -140,8 +156,8 @@ def continue_training(args):
 
     log_path = args.continue_from
     checkpoint_num = args.check_point_number
-    filename = "step_{}.pkl".format(checkpoint_num)
-    model_pkl = os.path.join("logs/", log_path, "checkpoints", filename)
+    filename = 'step_{}.pkl'.format(checkpoint_num)
+    model_pkl = os.path.join('logs/', log_path, 'checkpoints', filename)
     model_dict = torch.load(model_pkl)
     return model_dict, checkpoint_num
 
@@ -149,106 +165,52 @@ def continue_training(args):
 def parse_args(run_log_path):
     parser = argparse.ArgumentParser()
     # Run config
-    parser.add_argument("--gpu", help="Enable GPU use", action="store_true")
-    parser.add_argument(
-        "--max_iter",
-        type=int,
-        default=10000,
-        help="max number of iterations to train on",
-    )
+    parser.add_argument('--gpu', help='Enable GPU use', action='store_true')
+    parser.add_argument('--max_iter', type=int, default=10000,
+                        help='max number of iterations to train on')
 
     # Core Algorithm config
-    parser.add_argument(
-        "--z_pres",
-        type=str,
-        default="original_prior",
-        choices=["original_prior", "no_prior", "uniform_prior", "self_attention"],
-        help="name of the dataset",
-    )
+    parser.add_argument('--z_pres', type=str, default='original_prior',
+                        choices=['original_prior', 'no_prior', 'uniform_prior', 'self_attention'], help='name of the dataset')
 
-    parser.add_argument(
-        "--original_spair",
-        help="Uses sequential SPAIR rather than convolutional SPAIR",
-        action="store_true",
-    )
+    parser.add_argument('--original_spair', help='Uses sequential SPAIR rather than convolutional SPAIR',
+                        action='store_true')
 
-    parser.add_argument(
-        "--conv_neighbourhood", type=int, default=1, help="kernel size of conv_spair"
-    )
-    parser.add_argument(
-        "--use_z_where_decoder",
-        help="Use a decoder to model z_where better",
-        action="store_true",
-    )
+    parser.add_argument('--conv_neighbourhood', type=int, default=1,
+                        help='kernel size of conv_spair')
+    parser.add_argument('--use_z_where_decoder', help='Use a decoder to model z_where better', action='store_true')
 
-    parser.add_argument(
-        "--use_uber_trick",
-        help="Attaches explicit x,y information to input image for better localization",
-        action="store_true",
-    )
+    parser.add_argument('--use_uber_trick', help='Attaches explicit x,y information to input image for better localization', action='store_true')
 
-    parser.add_argument(
-        "--use_conv_z_attr",
-        help="Use a conv network to learn z_attr for faster learning",
-        action="store_true",
-    )
+    parser.add_argument('--use_conv_z_attr', help='Use a conv network to learn z_attr for faster learning', action='store_true')
 
-    parser.add_argument(
-        "--hw_prior",
-        type=float,
-        default=[3.0, 0.5],
-        nargs=2,
-        help="z prior for the height and width of bbox",
-    )
+    parser.add_argument('--hw_prior', type=float, default=[3., 0.5], nargs=2,
+                        help='z prior for the height and width of bbox')
 
-    parser.add_argument(
-        "--backbone_self_attention",
-        help="Use self attention for backbone network",
-        action="store_true",
-    )
+    parser.add_argument('--backbone_self_attention', help='Use self attention for backbone network', action='store_true')
 
     # Dataset config
-    parser.add_argument(
-        "--dataset_subset",
-        type=str,
-        default="constant",
-        choices=["constant", "full", "1-5"],
-        help="name of the dataset",
-    )
+    parser.add_argument('--dataset_subset', type=str, default='constant',
+                        choices=['constant', 'full', '1-5'], help='name of the dataset')
 
-    parser.add_argument(
-        "--dataset_filename",
-        type=str,
-        default="scattered_mnist_128x128_obj14x14.hdf5",
-        help="name of the dataset",
-    )
+    parser.add_argument('--dataset_filename', type=str, default='scattered_mnist_128x128_obj14x14.hdf5',
+                        help='name of the dataset')
 
     # Logging config
-    parser.add_argument(
-        "--log_path",
-        type=str,
-        default=run_log_path,
-        help="path of to store logging and checkpoints",
-    )
+    parser.add_argument('--log_path', type=str, default=run_log_path,
+                        help='path of to store logging and checkpoints')
 
     # Checkpoint restoration
-    parser.add_argument(
-        "--continue_from",
-        type=str,
-        default="",
-        help="name of run to continue trainig from",
-    )
+    parser.add_argument('--continue_from', type=str, default="",
+                        help='name of run to continue trainig from')
 
-    parser.add_argument(
-        "--check_point_number",
-        type=int,
-        default=0,
-        help="checkpoint number to load from",
-    )
+    parser.add_argument('--check_point_number', type=int, default=0,
+                        help='checkpoint number to load from')
 
     args = parser.parse_args()
     return args
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
